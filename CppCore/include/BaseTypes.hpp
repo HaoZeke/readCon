824ed328f16d22dbc25f224417dd3faa4f322840
Rohit Goswami <rog32@hi.is>
2023-07-21 17:18:04 +0000

    ENH: Setup structures for con files

--- CppCore/include/BaseTypes.hpp
+++ CppCore/include/BaseTypes.hpp
@@ -1,3 +1,27 @@
 #pragma once
 
-namespace yodecon::types {}
+#include <array>
+#include <string>
+#include <vector>
+
+namespace yodecon::types {
+
+struct AtomDatum {
+  char m_symbol;
+  double m_mass, m_x, m_y, m_z;
+  bool m_is_fixed;
+  size_t m_atom_id;
+};
+
+struct ConFrame {
+  std::array<std::string, 2> m_prebox_header;
+  std::array<double, 3> m_boxl;
+  std::array<double, 3> m_angles;
+  std::array<std::string, 2> m_postbox_header;
+  size_t m_natm_types;
+  std::vector<size_t> m_natms_per_type;
+  std::vector<size_t> m_masses_per_type;
+  std::vector<AtomDatum> m_atom_data;
+};
+
+} // namespace yodecon::types
