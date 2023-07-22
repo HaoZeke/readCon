65a67f2858b80cee8a1c975d7b7d2fedcd886871
Rohit Goswami <rog32@hi.is>
2023-07-22 02:59:30 +0000

    ENH: Add a usage example

--- CppCore/tiny_cli.cpp
+++ CppCore/tiny_cli.cpp
@@ -1,7 +1,15 @@
 #include "Helpers.hpp"
 #include "include/BaseTypes.hpp"
+#include "include/FormatConstants.hpp"
 #include "include/ReadCon.hpp"
-#include "include/StringHelpers.hpp"
+#include "include/helpers/StringHelpers.hpp"
+
+#ifdef WITH_FMT
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+#include <fmt/ranges.h>
+#endif
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -17,11 +25,33 @@ int main(int argc, char *argv[]) {
   // const size_t natoms_per_type_linum = 8;
   // const size_t masses_per_type_linum = 9;
 
-  auto fconts = yodecon::helpers::file::read_con_file(filename);
+  std::vector<std::string> fconts =
+      yodecon::helpers::file::read_con_file(filename);
 
-  for (auto &&line : fconts) {
-    std::cout << line << "\n";
+  yodecon::types::ConFrame tmp;
+
+  yodecon::process_header(
+      (fconts | std::views::take(yodecon::constants::HeaderLength)), tmp);
+
+#ifdef WITH_FMT
+  fmt::print("prebox_headers: {}\n", tmp.prebox_header);
+  fmt::print("box lengths: {}\n", tmp.boxl);
+  fmt::print("angles: {}\n", tmp.angles);
+  fmt::print("postbox_headers: {}\n", tmp.postbox_header);
+  fmt::print("natm_types: {}\n", tmp.natm_types);
+  fmt::print("natms_per_type: {}\n", tmp.natms_per_type);
+  fmt::print("masses_per_type: {}\n", tmp.masses_per_type);
+#endif
+
+  yodecon::process_coordinates(fconts, tmp);
+
+#ifdef WITH_FMT
+  fmt::print("{}\n", tmp.atom_data.size());
+  for (auto &&tmp_id : tmp.atom_data) {
+    fmt::print("{} {} {} {} {} {} {}\n", tmp_id.symbol, tmp_id.mass, tmp_id.x,
+               tmp_id.y, tmp_id.z, tmp_id.is_fixed, tmp_id.atom_id);
   }
+#endif
 
   return EXIT_SUCCESS;
 }
