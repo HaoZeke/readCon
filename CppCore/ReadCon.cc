ed7b668fe8c18b88d0dbcbbf5cda813470a6db2c
Rohit Goswami <rog32@hi.is>
2023-07-23 05:30:22 +0000

    MAINT: Be more explicit about casting from doubles

--- CppCore/ReadCon.cc
+++ CppCore/ReadCon.cc
@@ -14,9 +14,12 @@ void process_coordinates(const std::vector<std::string> &a_filecontents,
                        std::views::take(take_amount);
     tmp_atm.symbol = coords_view[0];
     for (auto &&line : coords_view | std::views::drop(constants::CoordHeader)) {
-      std::tie(tmp_atm.x, tmp_atm.y, tmp_atm.z, tmp_atm.is_fixed,
-               tmp_atm.atom_id) =
-          helpers::string::get_array_from_string<double, 5>(line);
+      auto dbl_line = helpers::string::get_array_from_string<double, 5>(line);
+      tmp_atm.x = dbl_line[0];
+      tmp_atm.y = dbl_line[1];
+      tmp_atm.z = dbl_line[2];
+      tmp_atm.is_fixed = static_cast<bool>(dbl_line[3]);
+      tmp_atm.atom_id = static_cast<size_t>(dbl_line[4]);
       conframe.atom_data.push_back(tmp_atm);
     }
     drop_amount += take_amount;
